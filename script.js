cf5d76ad7e141c2041ed263bcf6fec894f40e460
CSalerno22 <chris.salerno22@gmail.com>
2023-10-28 00:37:15 -0400

    Shopping List Local Storage Section

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
 const itemForm = document.getElementById('item-form')
 const itemInput = document.getElementById('item-input')
 const itemList = document.getElementById('item-list')
+const clearBtn = document.getElementById('clear')
+const itemFilter = document.getElementById('filter')
+let isEditMode = false
 
-function addItem(e) {
+function displayItems() {
+	const itemsFromStorage = getItemsFromStorage()
+	itemsFromStorage.forEach((item) => {
+		addItemToDOM(item)
+	})
+	checkUI()
+}
+
+function onAddItemSubmit(e) {
 	e.preventDefault()
 	const newItem = itemInput.value.trim()
 
@@ -12,15 +23,26 @@ function addItem(e) {
 		return
 	}
 
+	//Create item DOM element
+	addItemToDOM(newItem)
+
+	//Add item to local storage
+	addItemToStorage(newItem)
+
+	checkUI()
+	itemInput.value = ''
+}
+
+function addItemToDOM(item) {
 	//Create list Item
 	const li = document.createElement('li')
-	li.appendChild(document.createTextNode(newItem))
+	li.appendChild(document.createTextNode(item))
 
 	const button = createButton('remove-item btn-link text-red')
 	li.appendChild(button)
 
+	//Adding LI to DOM
 	itemList.appendChild(li)
-	itemInput.value = ''
 }
 
 function createButton(classes) {
@@ -37,5 +59,111 @@ function createIcon(classes) {
 	return icon
 }
 
-//Event Listeners
-itemForm.addEventListener('submit', addItem)
+function addItemToStorage(item) {
+	const itemsFromStorage = getItemsFromStorage()
+
+	//Add neww item to array
+	itemsFromStorage.push(item)
+
+	//Convert to Json string and set to local storage
+	localStorage.setItem('items', JSON.stringify(itemsFromStorage))
+}
+
+function getItemsFromStorage() {
+	let itemsFromStorage
+
+	if (localStorage.getItem('items') === null) {
+		itemsFromStorage = []
+	} else {
+		itemsFromStorage = JSON.parse(localStorage.getItem('items'))
+	}
+	return itemsFromStorage
+}
+
+function onClickItem(e) {
+	if (e.target.parentElement.classList.contains('remove-item')) {
+		removeItem(e.target.parentElement.parentElement)
+	} else {
+		setItemToEdit(e.target)
+	}
+}
+
+funtion setItemToEdit(item){
+	isEditMode = true
+	
+}
+
+function removeItem(item) {
+	console.log(item)
+
+	if (confirm('Are you sure you want to remove this item?')) {
+		//Remove item from DOM
+		item.remove()
+
+		//Remove item from storage
+		removeItemFromStorage(item.textContent)
+
+		checkUI()
+	}
+}
+
+function removeItemFromStorage(item) {
+	let itemsFromStorage = getItemsFromStorage()
+
+	//Filter out item to be removed
+	itemsFromStorage = itemsFromStorage.filter((i) => i !== item)
+
+	//Reset to local stoRAGE
+	localStorage.setItem('items', JSON.stringify(itemsFromStorage))
+}
+
+function clearItems() {
+	while (itemList.firstChild) {
+		itemList.removeChild(itemList.firstChild)
+	}
+
+	//Clear from local storage
+	localStorage.removeItem('items')
+	checkUI()
+}
+
+function filterItems(e) {
+	const items = itemList.querySelectorAll('li')
+	const text = e.target.value.toLowerCase()
+
+	items.forEach((item) => {
+		const itemName = item.firstChild.textContent.toLowerCase()
+
+		if (itemName.indexOf(text) != -1) {
+			item.style.display = 'flex'
+		} else {
+			item.style.display = 'none'
+		}
+	})
+}
+
+function checkUI() {
+	const items = itemList.querySelectorAll('li')
+
+	if (items.length === 0) {
+		clearBtn.style.display = 'none'
+		itemFilter.style.display = 'none'
+	} else {
+		clearBtn.style.display = 'block'
+		itemFilter.style.display = 'block'
+	}
+}
+
+//Initialize app
+function init() {
+	//Event Listeners
+	itemForm.addEventListener('submit', onAddItemSubmit)
+	itemList.addEventListener('click', onClickItem)
+	clearBtn.addEventListener('click', clearItems)
+	itemFilter.addEventListener('input', filterItems)
+	document.addEventListener('DOMContentLoaded', displayItems)
+
+	checkUI()
+}
+
+init()
